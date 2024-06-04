c4468ca2fc7393d37bc097c4bbdc53d07ed5fa31
leorvergani <vergani@outlook.com.br>
2024-06-04 00:47:26 -0300

    update da aula ao vivo

--- aula21/aula-style-classList/script.js
+++ aula21/aula-style-classList/script.js
@@ -1,3 +1,11 @@
+//console.log("Olá, mundo!")
+
+const h2Element = document.querySelector("h2")
+
+h2Element.style.color = "blue"
+h2Element.style.fontSize = "30px"
+
+
 const buttonElement = document.querySelector("button")
 
 buttonElement.style.backgroundColor = "red"
@@ -7,11 +15,22 @@ buttonElement.style.borderRadius = "4px"
 
 // PRIMEIRA INTERAÇÃO DO USUÁRIO - CAPTURA O INPUT DE NOME DO USUÁRIO
 const userNameInput = document.getElementById("login-usuario")
+
 // ADICIONA A CLASSE "ERROR" AO INPUT DE NOME DO USUÁRIO
-userNameInput.classList.add("error")
+userNameInput.classList.remove("error")
+
+// SEGUNDA INTERAÇÃO DO USUÁRIO - CORRIGE O INPUT DE NOME DO USUÁRIO
+userNameInput.classList.add("correct")
+
+// E ADICIONA A CLASSE "ERROR"  AO INPUT DE SENHA
+const passwordInput = document.getElementById("login-senha")
+passwordInput.classList.add("error")
 
 // CAPTURA A PRIMEIRA MENSAGEM DE ERRO
 const errorMessage = document.querySelector(".error-text")
+
 // ADICIONA A CLASSE "VISIBLE" À MENSAGEM DE ERRO
-errorMessage.classList.add("visible")
+errorMessage[0].classList.remove("visible")
+// ADICIONA A CLASSE "VISIBLE" À SEGUNDA MENSAGEM DE ERRO
+errorMessage[1].classList.add("visible")
 
