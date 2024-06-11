eacdb5343807cd9e3b232f7264870991a6f46be4
leorvergani <vergani@outlook.com.br>
2024-06-11 00:23:05 -0300

    Terminando outra aula da Proz

--- aula26_Proz_JS/aula-event-listener-eventos-mouse/script.js
+++ aula26_Proz_JS/aula-event-listener-eventos-mouse/script.js
@@ -1,10 +1,28 @@
+// Captura de elementos do DOM (Variáveis)
 let quantidadeSubtotal = document.getElementById("quantidade-subtotal");
 let valorSubtotal = document.getElementById("valor-subtotal");
+let btnAdicionarProduto01 = document.getElementById("btn-adicionar-produto-01");
+let qtdProduto01 = document.getElementById("quantidade-produto-01");
+let btnSubtrairProduto01 = document.getElementById("btn-subtrair-produto-01");
 
 let subtotalInfo = {
   quantidade: 1,
   valor: 11.66,
 };
 
+// Definir a manipular dos elementos capturados (Funções)
 quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
 valorSubtotal.innerText = subtotalInfo.valor;
+
+function adicionarUM(){
+  qtdProduto01.value = Number(qtdProduto01.value) + 1
+}
+
+function subtrairUm(){
+  qtdProduto01.value = Number(qtdProduto01.value) - 1
+}
+
+
+// Definir quando devem ser manipulador os elementos (Eventos)
+btnAdicionarProduto01.addEventListener("click", adicionarUM)
+btnSubtrairProduto01.addEventListener("click", subtrairUm)
